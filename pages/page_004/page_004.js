6423ed3976def0e6631bd7004969f311b6f581d5
DavidDang <dy_sx@163.com>
2018-05-11 18:34:23 +0800

    task page_004: complete the major UI, need to refine the layout

--- pages/page_004/page_004.js
+++ pages/page_004/page_004.js
@@ -5,7 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    days: 14,
+    selectedDate: '',//选中的几月几号
+    selectedWeek: '',//选中的星期几
+    curYear: 2017,//当前年份
+    curMonth: 0,//当前月份
+    daysCountArr: [// 保存各个月份的长度，平年
+      31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    ],
+    weekArr: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+    dateList: []
   },
 
   /**
@@ -13,7 +22,7 @@ Page({
    */
   onLoad: function (options) {
     wx.setNavigationBarTitle({
-      title: '学习日历'
+      title: '打卡日历'
     })
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
@@ -25,48 +34,105 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
-  },
+    var today = new Date();//当前时间  
+    var y = today.getFullYear();//年  
+    var mon = today.getMonth() + 1;//月  
+    var d = today.getDate();//日  
+    var i = today.getDay();//星期  
+    this.setData({
+      curYear: y,
+      curMonth: mon,
+      selectedDate: y + '-' + mon + '-' + d,
+      selectedWeek: this.data.weekArr[i]
+    });
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+    this.getDateList(y, mon - 1);
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  getDateList: function (y, mon) {
+    var vm = this;
+    //如果是否闰年，则2月是29日
+    var daysCountArr = this.data.daysCountArr;
+    if (y % 4 == 0 && y % 100 != 0) {
+      this.data.daysCountArr[1] = 29;
+      this.setData({
+        daysCountArr: daysCountArr
+      });
+    }
+    //第几个月；下标从0开始实际月份还要再+1  
+    var dateList = [];
+    // console.log('本月', vm.data.daysCountArr[mon], '天');
+    dateList[0] = [];
+    var weekIndex = 0;//第几个星期
+    for (var i = 0; i < vm.data.daysCountArr[mon]; i++) {
+      var week = new Date(y, mon, (i + 1)).getDay();
+      // 如果是新的一周，则新增一周
+      if (week == 0) {
+        weekIndex++;
+        dateList[weekIndex] = [];
+      }
+      // 如果是第一行，则将该行日期倒序，以便配合样式居右显示
+      if (weekIndex == 0) {
+        dateList[weekIndex].unshift({
+          value: y + '-' + (mon + 1) + '-' + (i + 1),
+          date: i + 1,
+          week: week
+        });
+      } else {
+        dateList[weekIndex].push({
+          value: y + '-' + (mon + 1) + '-' + (i + 1),
+          date: i + 1,
+          week: week
+        });
+      }
+    }
+    // console.log('本月日期', dateList);
+    vm.setData({
+      dateList: dateList
+    });
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+  selectDate: function (e) {
+    var vm = this;
+    // console.log('选中', e.currentTarget.dataset.date.value);
+    vm.setData({
+      selectedDate: e.currentTarget.dataset.date.value,
+      selectedWeek: vm.data.weekArr[e.currentTarget.dataset.date.week]
+    });
   },
+  preMonth: function () {
+    // 上个月
+    var vm = this;
+    var curYear = vm.data.curYear;
+    var curMonth = vm.data.curMonth;
+    curYear = curMonth - 1 ? curYear : curYear - 1;
+    curMonth = curMonth - 1 ? curMonth - 1 : 12;
+    // console.log('上个月', curYear, curMonth);
+    vm.setData({
+      curYear: curYear,
+      curMonth: curMonth
+    });
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+    vm.getDateList(curYear, curMonth - 1);
   },
+  nextMonth: function () {
+    // 下个月
+    var vm = this;
+    var curYear = vm.data.curYear;
+    var curMonth = vm.data.curMonth;
+    curYear = curMonth + 1 == 13 ? curYear + 1 : curYear;
+    curMonth = curMonth + 1 == 13 ? 1 : curMonth + 1;
+    // console.log('下个月', curYear, curMonth);
+    vm.setData({
+      curYear: curYear,
+      curMonth: curMonth
+    });
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    vm.getDateList(curYear, curMonth - 1);
   }
 })
