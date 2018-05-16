794bbee495c0c6bcbdb80eca8e506f79ca0c732e
David <dy_sx@163.com>
2018-05-16 18:45:03 +0800

    task page_002: integrate pronounce API; change FE data

--- pages/page_002/page_002.js
+++ pages/page_002/page_002.js
@@ -12,33 +12,25 @@ Page({
     wordlist: ["Financial", "daily", "exercise", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
     wordidlist: [],
     worddetail: {
-      pronounce: ['[faɪˈnænʃl]', '[faɪˈnænʃ(ə)l]'],
-      translation: ['adj.  财政的; 财务的; 金融的; 有钱的;'],
-      sentence: ['1. The enquiry dug deeper into the alleged financial misdeeds of his government.']
+      pronounce: '[faɪˈnænʃ(ə)l]',
+      translation: 'adj.  财政的; 财务的; 金融的; 有钱的;',
+      sentence: '1. The enquiry dug deeper into the alleged financial misdeeds of his government.'
     },
     wordlistdetail: [
       {
-        pronounce: ['[faɪˈnænʃl]', '[faɪˈnænʃ(ə)l]'],
-        translation: ['adj.  财政的; 财务的; 金融的; 有钱的;'],
-        sentence: ['1. The enquiry dug deeper into the alleged financial misdeeds of his government.']
+        pronounce: '[faɪˈnænʃ(ə)l]',
+        translation: 'adj.  财政的; 财务的; 金融的; 有钱的;',
+        sentence: '1. The enquiry dug deeper into the alleged financial misdeeds of his government.'
       },
       {
-        pronounce: ['[ˈdeɪli]', '[ˈdeli]'],
-        translation: [
-          'adj.  每日的，日常的; 一日的; 每日一次的; 每个工作日的;',
-          'n.  日报; （不寄宿的）仆人，白天做家务的女佣;',
-          'adv.  每日; 逐日; 每周日; 日复一日地;'
-        ],
-        sentence: ['1. Daily facial exercises help her to retain the skin\'s elasticity.']
+        pronounce: '[ˈdeli]',
+        translation: 'adj.  每日的，日常的; 一日的; 每日一次的; 每个工作日的;',
+        sentence: '1. Daily facial exercises help her to retain the skin\'s elasticity.'
 
       }, {}, {}, {}, {}, {}, {}, {}, {
-        pronounce: ['[ˈsʌndeɪ]', '[ˈsʌnˌdeɪ]'],
-        translation: [
-          'n.星期日，星期天; 每逢星期日出版的报纸; 星期日报; [人名] 森迪;'
-        ],
-        sentence: ['1. On Sunday Cohen lay around the house all day. ',
-          '2. Randall would just now be getting the Sunday paper.'
-        ]
+        pronounce: '[ˈsʌnˌdeɪ]',
+        translation: 'n.星期日，星期天; 每逢星期日出版的报纸; 星期日报; [人名] 森迪;',
+        sentence: '1. On Sunday Cohen lay around the house all day. '
 
       }
     ]
@@ -58,108 +50,105 @@ Page({
   //手指滑动开始
   mytouchend: function (event) {
     var distancex = event.changedTouches[0].pageX - this.data.startx;
-    var i = this.data.wordindex;
-    var wi = i;
+    var wi = this.data.wordindex;
+
+    //update word index
     var refer = this;
-    if (distancex < 0) {//move forward
-      if (i >= this.data.wordlist.length - 1) {
+    if (distancex < 0) {//left move
+      if (wi >= this.data.wordlist.length - 1) {
         wi = 0;
-        /*this.setData({
-          wordindex: 0
-        })*/
       } else {
-        wi = this.data.wordindex + 1;
-        /*this.setData({
-          wordindex: this.data.wordindex + 1
-        })*/
+        wi++;
       }
-    } else if (distancex > 0) { // move back
-      if (i <= 0) {
+    } else if (distancex > 0) { // move right
+      if (wi <= 0) {
         wi = this.data.wordlist.length - 1;
-        /*this.setData({
-          wordindex: this.data.wordlist.length - 1
-        })*/
       } else {
-        wi = this.data.wordindex - 1;
-        /*this.setData({
-          wordindex: this.data.wordindex - 1
-        })*/
+        wi--;
       }
     }
+    if (distancex != 0) {
+      //get detail of current word
+      if (this.data.pagestyle == 'complex') {
+        console.log("call backend, pagestyle == 'complex'");
+        var resp = null;
+        wx.request({
+          url: 'https://aisss5ct.qcloud.la/Emp/mobile/bearword/mean/' + this.data.wordidlist[wi],
+          method: 'GET',
 
-    //get detail of current word
-    var tmp = null;
-    wx.request({
-      url: 'https://aisss5ct.qcloud.la/Emp/mobile/bearword/mean/' + this.data.wordidlist[wi],
-      method: 'POST',
-      header: {
-        "Content-Type": "application/json"
-      },
-      data: {
-      },
-      success: function (resz) {
-        tmp = {
-          translation: [resz.data.interpretation],
-          sentence: [resz.data.sentence],
-        }
-        
-        refer.setData({
-          wordindex: wi,
-          worddetail: tmp
+          success: function (resz) {
+            resp = {
+              pronounce: resz.data.symbol,
+              translation: resz.data.interpretation,
+              sentence: resz.data.sentence
+            }
+            refer.setData({
+              wordindex: wi,
+              worddetail: resp
+            })
+          }
         })
+      } else {
+        refer.setData({
+          wordindex: wi
+        })
+      }
+    }
+  },
+
+  //单词读音
+  speech: function () {
+    var refer = this;
+    wx.request({
+      url: 'https://aisss5ct.qcloud.la/Emp/mobile/word/pronunciation/' + refer.data.wordlist[refer.data.wordindex],
+      method: 'GET',
+      success: function (res) {
+        const backgroundAudioManager = wx.getBackgroundAudioManager()
+        backgroundAudioManager.src = res.data
       }
     })
   },
-
 
   //显示详细释义
-  todetail: function (event) {
+  changestyle: function (event) {
 
-    var tmp = null;
-    var refer = this;
+    if (this.data.pagestyle == 'simple') {
+      var resp = null;
+      var refer = this;
 
-    wx.request({
-      url: 'https://aisss5ct.qcloud.la/Emp/mobile/bearword/mean/' + this.data.wordidlist[0],
-      method: 'POST',
-      header: {
-        "Content-Type": "application/json"
-      },
-      data: {
-        //wid: "8e8ba7beee5042ab8c7b853661941616"
-      },
-      success: function (resz) {
-        tmp = {
-          translation: [resz.data.interpretation],
-          sentence: [resz.data.sentence],
+      wx.request({
+        url: 'https://aisss5ct.qcloud.la/Emp/mobile/bearword/mean/' + refer.data.wordidlist[refer.data.wordindex],
+        method: 'GET',
+
+        success: function (resz) {
+          resp = {
+            pronounce: resz.data.symbol,
+            translation: [resz.data.interpretation],
+            sentence: [resz.data.sentence],
+          }
+
+          refer.setData({
+            pagestyle: 'complex',
+            worddetail: resp
+          })
         }
-
-        refer.setData({
-          pagestyle: 'complex',
-          worddetail: tmp
-        })
-      }
-    })
+      })
+    } else if (this.data.pagestyle == 'complex') {
+      this.setData({
+        pagestyle: 'simple'
+      })
+    }
   },
 
-  tosimplestyle: function (event) {
-    this.setData({
-      pagestyle: 'simple'
-    })
-  },
   /**
-   * 生命周期函数--监听页面加载
-   */
+  * 生命周期函数--监听页面加载
+  */
   onLoad: function (options) {
     var refer = this;
     wx.request({
       url: 'https://aisss5ct.qcloud.la/Emp/mobile/bearword/query/020b28e556de4352a231650c1637653c',
-      method: 'POST',
-      header: {
-        "Content-Type": "application/json"
-      },
-      data: {
-        //uid: "020b28e556de4352a231650c1637653c"
-      },
+      method: 'GET',
+
       success: function (resz) {
         var acc = resz.data.total;
         var wordarray = new Array(acc);
