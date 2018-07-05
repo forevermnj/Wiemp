57eb31a1f51366d76d869c7f2bb53328448ad545
dapengchina <aoqishequ@163.com>
2018-07-05 14:29:28 +0800

    commit code

--- pages/page_010/page_010.js
+++ pages/page_010/page_010.js
@@ -3,55 +3,43 @@ Page({
     list1: [
       {
         pic: '../image/page_010/1.png',
-        title: '背单词'
+        title: 'sweet words'
       }, {
         pic: '../image/page_010/2.png',
-        title: '导师'
+        title: 'mongo reading'
       }, {
         pic: '../image/page_010/3.png',
-        title: '场景'
+        title: 'stand up meeting'
       }, {
         pic: '../image/page_010/4.png',
-        title: '场景'
+        title: 'stand up meeting'
       }, {
         pic: '../image/page_010/5.png',
-        title: '阅读'
+        title: 'Professional literacy'
       }
     ],
     list2: [
       {
         pic: '../image/page_010/6.png',
-        title: 'Mule'
+        title: 'mule'
       }, {
         pic: '../image/page_010/7.png',
-        title: 'Aws'
+        title: 'aws'
       }, {
         pic: '../image/page_010/8.png',
-        title: '敬请期待'
+        title: 'expect'
       }, {
         pic: '../image/page_010/9.png',
-        title: 'Stand Up'
+        title: 'stand Up'
       }, {
         pic: '../image/page_010/10.png',
-        title: 'MuleSoft'
+        title: 'mulesoft'
       }
     ],
     list3: [
       {
         pic: '../image/page_010/11.png',
-        title: '敬请期待'
-      }, {
-        pic: '../image/page_010/11.png',
-        title: '敬请期待'
-      }, {
-        pic: '../image/page_010/11.png',
-        title: '敬请期待'
-      }, {
-        pic: '../image/page_010/11.png',
-        title: '敬请期待'
-      }, {
-        pic: '../image/page_010/11.png',
-        title: '敬请期待'
+        title: 'expect'
       }
     ],
     headImage: wx.getStorageSync('headImage'),
