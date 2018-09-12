74a3d5c553664ac8f0bd4bf87180107d631f0f3c
dapengchina <aoqishequ@163.com>
2018-09-12 10:02:42 +0800

    commit code

--- pages/page_017/page_017.js
+++ pages/page_017/page_017.js
@@ -1,114 +1,72 @@
 var app = getApp();
 var x, y, x1, y1, x2, y2, index, currindex, n, yy;
-var arr1 =
-  [
-    { content: 'There is no apparent leader or subordinate, everyone can measure with the corner of eyes to judge if others are interested in our topics when we make a circle.', id: 2, key: 2 },
-    { content: 'Last but not least, we should hold the stand-up meeting in a fixed, public place to ensure team belongings can be built and team spirit can be shown.', id: 4, key: 4 },
-    { content: 'When we stand up to conduct the meeting, it will turn out to be much faster and more efficient than a sitting way', id: 1, key: 1 },
-    { content: 'How to to form the meeting habit and discipline? To fix the stand up meeting time in 15 minutes, so that staff can assemble together to communicate efficiently.', id: 3, key: 3 }
-  ];
+var arr1 = [];
 Page({
   data: {
     mainx: 0,
     indeximg: '../image/tabbar/2.png',
     previousImg: '../image/tabbar/13.png',
-    content: [
-      { content: 'There is no apparent leader or subordinate, everyone can measure with the corner of eyes to judge if others are interested in our topics when we make a circle.', id: 2, key: 2 },
-      { content: 'Last but not least, we should hold the stand-up meeting in a fixed, public place to ensure team belongings can be built and team spirit can be shown.', id: 4, key: 4 },
-      { content: 'When we stand up to conduct the meeting, it will turn out to be much faster and more efficient than a sitting way', id: 1, key: 1},
-      { content: 'How to to form the meeting habit and discipline? To fix the stand up meeting time in 15 minutes, so that staff can assemble together to communicate efficiently.', id: 3, key: 3},
-      
-    ],
+    originContent:{},
+    content: [],
     start: { x: 0, y: 0 },
-    correctFlag:false,
-    mp3index:0,
-    allowClickNum:3,
-    allowClickIndex:0,
-    mp3:[
+    correctFlag: false,
+    mp3index: 0,
+    allowClickNum: 3,
+    allowClickIndex: 0,
+    mp3: [
       app.globalData.serverUrl + '/Emp/mobile/mp3/3.mp3',
       app.globalData.serverUrl + '/Emp/mobile/mp3/page_017/1.mp3'
     ]
   },
-  toSubmit:function(){
-     let refer = this;
-     //console.log('提交');
-     //console.log(refer.data.content);
+  toSubmit: function () {
+    let refer = this;
+    //console.log('提交');
+    //console.log(refer.data.content);
     if (
-      refer.data.content[0].key==1 &&
-      refer.data.content[1].key==2 &&
-      refer.data.content[2].key==3 &&
-      refer.data.content[3].key==4
-      ){
-         //排序正确
-         refer.setData({
-           correctFlag:true
-         });
-      // app.globalData.backImgIndex = app.globalData.backImgIndex+1;
-      // app.globalData.backMp3Index = app.globalData.backMp3Index+1;
-      //page_012页面全局参数
-      app.globalData.backImgIndex = app.globalData.backImgIndex+1;
-      //page_012页面全局参数
-      app.globalData.backMp3Index = app.globalData.backMp3Index+1;
-      //page_013页面全局参数
-      app.globalData.chooseDataIndex = app.globalData.chooseDataIndex+2;
-      //page_013页面全局参数
-      app.globalData.mp3dataIndex = app.globalData.mp3dataIndex+2;
-      //page_014页面全局参数
-      app.globalData.dataIndex = app.globalData.dataIndex+2;
-      //page_014页面全局参数
-      app.globalData.anwdataIndex = app.globalData.anwdataIndex+2;
-      //page_015页面全局参数
-      app.globalData.mp3dataIndex2 = app.globalData.mp3dataIndex2+1;
-      //page_016页面全局参数
-      app.globalData.rdataIndex = app.globalData.rdataIndex+2;
+      refer.data.content[0].key == 1 &&
+      refer.data.content[1].key == 2 &&
+      refer.data.content[2].key == 3 &&
+      refer.data.content[3].key == 4
+    ) {
+      //排序正确
+      refer.setData({
+        correctFlag: true
+      });
+      
       let tempFilePath = refer.data.mp3[refer.data.mp3index];
       wx.playBackgroundAudio({
         dataUrl: tempFilePath
       });
       wx.onBackgroundAudioStop(function () {
         refer.setData({
-          mp3index:refer.data.mp3index+1
+          mp3index: refer.data.mp3index + 1
         })
-        if (refer.data.mp3index==2){
+        if (refer.data.mp3index == 2) {
+          let path = refer.data.originContent.sortdata.dropLetLink;
+          app.globalData.dropLetId = refer.data.originContent.sortdata.reladropletid;
+          app.globalData.dropLetConfigTypeId = refer.data.originContent.sortdata.reladropletconftypeid;
           wx.redirectTo({
-            url: '../page_012/page_012',
+            url: path
           });
-        }else{
+        } else {
           let tempFilePath = refer.data.mp3[refer.data.mp3index];
           wx.playBackgroundAudio({
             dataUrl: tempFilePath
           });
         }
-        
+
       });
-      
-    }else{
+
+    } else {
       //排错次数递增
       refer.data.allowClickIndex = refer.data.allowClickIndex + 1;
-      if (refer.data.allowClickIndex == refer.data.allowClickNum){
+      if (refer.data.allowClickIndex == refer.data.allowClickNum) {
         //排序正确
         refer.setData({
           correctFlag: true
         });
         refer.data.allowClickIndex = 0;
-        // app.globalData.backImgIndex = app.globalData.backImgIndex+1;
-        // app.globalData.backMp3Index = app.globalData.backMp3Index+1;
-        //page_012页面全局参数
-        app.globalData.backImgIndex = app.globalData.backImgIndex + 1;
-        //page_012页面全局参数
-        app.globalData.backMp3Index = app.globalData.backMp3Index + 1;
-        //page_013页面全局参数
-        app.globalData.chooseDataIndex = app.globalData.chooseDataIndex + 2;
-        //page_013页面全局参数
-        app.globalData.mp3dataIndex = app.globalData.mp3dataIndex + 2;
-        //page_014页面全局参数
-        app.globalData.dataIndex = app.globalData.dataIndex + 2;
-        //page_014页面全局参数
-        app.globalData.anwdataIndex = app.globalData.anwdataIndex + 2;
-        //page_015页面全局参数
-        app.globalData.mp3dataIndex2 = app.globalData.mp3dataIndex2 + 1;
-        //page_016页面全局参数
-        app.globalData.rdataIndex = app.globalData.rdataIndex + 2;
+        
         let tempFilePath = refer.data.mp3[refer.data.mp3index];
         wx.playBackgroundAudio({
           dataUrl: tempFilePath
@@ -118,8 +76,11 @@ Page({
             mp3index: refer.data.mp3index + 1
           })
           if (refer.data.mp3index == 2) {
+            let path = refer.data.originContent.sortdata.dropLetLink;
+            app.globalData.dropLetId = refer.data.originContent.sortdata.reladropletid;
+            app.globalData.dropLetConfigTypeId = refer.data.originContent.sortdata.reladropletconftypeid;
             wx.redirectTo({
-              url: '../page_012/page_012',
+              url: path
             });
           } else {
             let tempFilePath = refer.data.mp3[refer.data.mp3index];
@@ -200,7 +161,7 @@ Page({
       arr1 = [];
       for (var m = 0; m < this.data.content.length; m++) {
         //console.log(arr[m]);
-        arr[m].id = m + 1;
+        arr[m].num = m + 1;
         arr1.push(arr[m]);
       }
       // console.log(arr1);
@@ -210,5 +171,32 @@ Page({
         opacity: 1
       })
     }
+  },
+  onLoad: function () {
+    //util.showBusy('加载中...');
+    let refer = this;
+    wx.request({
+      url: app.globalData.serverUrl + '/Emp/mobile/getSortData/getSortData/' + app.globalData.dropLetId + '/' + app.globalData.dropLetConfigTypeId,
+      method: 'GET',
+      success: function (res) {
+        refer.setData({
+          content: res.data.sortdata.sentence,
+          originContent: res.data
+        });
+        arr1 = res.data.sortdata.sentence;
+      }
+    })
+
+  },
+  toBootomButton: function (e) {
+    let refer = this;
+    let csv0 = e.currentTarget.dataset.hi[0];
+    let csv1 = e.currentTarget.dataset.hi[1];
+    let csv2 = e.currentTarget.dataset.hi[2];
+    app.globalData.dropLetId = csv1;
+    app.globalData.dropLetConfigTypeId = csv2;
+    wx.redirectTo({
+      url: csv0,
+    });
   }
 })
