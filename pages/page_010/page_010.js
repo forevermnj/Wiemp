feacbe83f4de81134b60471e6624a927fd6336d3
dapengchina <aoqishequ@163.com>
2018-10-26 14:11:55 +0800

    commit code

--- pages/page_010/page_010.js
+++ pages/page_010/page_010.js
@@ -14,21 +14,27 @@ Page({
     let csv1 = e.currentTarget.dataset.hi[1];
     let csv2 = e.currentTarget.dataset.hi[2];
     let csv3 = e.currentTarget.dataset.hi[3];
+    let csv4 = e.currentTarget.dataset.hi[4];
     app.globalData.dropLetId = csv1;
     app.globalData.dropLetConfigTypeId = csv2;
     app.globalData.index = csv3;
-    wx.redirectTo({
-       url: csv0,
-    });
-   
-  },
-  onPullDownRefresh:function(){
-    
+
+    if (csv4){
+      wx.redirectTo({
+        url: csv0,
+      });
+    }else{
+      wx.showToast({
+        title: '无权限',
+        image: '../image/tabbar/30.png',
+        duration: 1000
+      })
+    }
   },
   onLoad: function () {
     let refer = this;
     wx.request({
-      url: app.globalData.serverUrl + '/Emp/mobile/getIndexData/getIndexData',
+      url: app.globalData.serverUrl + '/Emp/mobile/getIndexData/getIndexData/' + wx.getStorageSync('uid'),
       method: 'GET',
       success: function (res) {
         refer.setData({
@@ -38,52 +44,8 @@ Page({
     })
     util.showBusy('加载中...');
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
     util.showSuccess('加载成功');
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   },
   toIndex:function(){
      let refer = this;
